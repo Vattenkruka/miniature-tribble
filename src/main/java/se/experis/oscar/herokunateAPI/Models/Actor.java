df7416278a1b34471fc77c80a89449a478095e2c
Oscar Dahlquist <Oscar.Dahlquist@gmail.com>
2020-10-22 11:38:13 +0200

    Vattenkruka: Setup

--- src/main/java/se/experis/oscar/herokunateAPI/Models/Actor.java
+++ src/main/java/se/experis/oscar/herokunateAPI/Models/Actor.java
@@ -15,14 +15,53 @@ public class Actor {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    public Integer id;
+    private Integer id;
 
     @Column(nullable=false)
-    public String firstname;
+    private String firstname;
 
     @Column(nullable = false)
-    public String lastName;
+    private String lastName;
 
     @Column(nullable = false)
-    public String url;
+    private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Actor(Integer id, String firstname, String lastName, String url) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.url = url;
+    }
 }
